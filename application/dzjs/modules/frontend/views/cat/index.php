b59d7747a2a17c8c64397ee772cbac3beceed285
whis <wqcsimple@foxmail.com>
2016-01-04 19:32:47 +0800

    update

--- application/dzjs/modules/frontend/views/cat/index.php
+++ application/dzjs/modules/frontend/views/cat/index.php
@@ -5,32 +5,20 @@ use fayfox\helpers\Date;
 <link rel="stylesheet" type="text/css" href="<?php echo $this->staticFile('css/fuye.css')?>">
  <div class="alltop"><?php F::app()->widget->load('list-banner-image')?></div>
  <div class="aio">
+     <div class="gyleft">
+          <?php F::app()->widget->load('hot-news')?>
+          <?php F::app()->widget->load('list-zlxz')?>
+     </div>
 
- <div class="gyleft">
-
-
-<?php F::app()->widget->load('hot-news')?>
-<?php F::app()->widget->load('list-zlxz')?>
-  
 <!----------------附页公用左侧----------------------> 
-
- <div class="gyright">
-
-   
-   <div class="gyright_head1"><a href="<?php echo $this->url()?>">首页</a>><a href="<?php echo $this->url('cat/'.$cat['id'])?>" ><?php echo $cat['title']?></a>
-
-  </div>
-
-  
-               
-   
-   <ul class="tzgg">
-
-	<?php $listview->showData();?>
-   </ul>
-
-	<?php $listview->showPage();?>
-    
+     <div class="gyright">
+       <div class="gyright_head1"><a href="<?php echo $this->url()?>">首页</a>><a href="<?php echo $this->url('cat/'.$cat['id'])?>" ><?php echo $cat['title']?></a>
+       </div>
+       <ul class="tzgg">
+         <?php $listview->showData();?>
+       </ul>
+        <?php $listview->showPage();?>
+     </div>
   </div>
  
 <!----------------附页公用右侧----------------------> 
--- application/dzjs/modules/frontend/views/page/item.php
+++ application/dzjs/modules/frontend/views/page/item.php
@@ -11,22 +11,17 @@ use fayfox\helpers\Date;
 <?php F::app()->widget->load('hot-news')?>
 
 <?php F::app()->widget->load('list-zlxz')?>
-  
+ </div>
 <!----------------附页公用左侧----------------------> 
  <div class="gyright">
   <div class="gyright_head"><a href="<?php echo $this->url()?>">首页</a>
   	  ><a href="" >教学队伍介绍</a>
-	  	
-	      </div>
-  <div class="zwd">
-  <div class="zw">
-   <h2><?php echo Html::encode($about['title'])?></h2>
-     <h3></h3>
-		
-			<?php echo $about['content']?>
-		
-
   </div>
+  <div class="zwd">
+      <div class="zw">
+       <h2><?php echo Html::encode($about['title'])?></h2>
+          <?php echo $about['content']?>
+      </div>
   </div>
  </div>
 
